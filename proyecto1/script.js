ab69362f760fa10c9a60295e02df8d7767fdf491
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2025-02-12 18:15:04 +0100

    dia 2

--- proyecto1/script.js
+++ proyecto1/script.js
@@ -15,3 +15,40 @@ function logout(){
     toogleLogout();
     console.log('Acabas de hacer el Logout');
 }
+
+
+
+function mostrarToast(id, titulo, mensaje){
+    console.log('Vamos a mostrar un toast');
+    const container = ensureToastContainer();
+
+    const template = document.getElementById('toast-template');
+    const nuevoToast = template.content.cloneNode(true);
+
+    // Le asigno el titulo y el texto al toast
+    nuevoToast.querySelector('h3').textContent = titulo;
+    nuevoToast.querySelector('p').innerHTML = mensaje;
+    const divToast =  nuevoToast.querySelector('.toast');
+    divToast.id = id;
+    divToast.addEventListener('click', () => closeToast(id) );
+
+    container.appendChild(nuevoToast);
+
+    //<div id="toast-container"></div>
+}
+
+function closeToast(toastId){
+    const toast = document.getElementById(toastId);
+    if(toast)
+        ensureToastContainer().removeChild(toast);
+}
+
+function ensureToastContainer(){
+    let toastContainer = document.getElementById('toast-container');
+    if( !toastContainer ){
+        toastContainer = document.createElement('div');
+        toastContainer.id = 'toast-container';
+        document.body.appendChild(toastContainer);
+    }
+    return toastContainer;
+}
