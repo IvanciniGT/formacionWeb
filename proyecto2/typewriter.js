d8bcbdb4b2814f0e3133eb3ca448361572fd642d
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2025-02-13 19:22:46 +0100

    dia4

--- proyecto2/typewriter.js
+++ proyecto2/typewriter.js
@@ -1,4 +1,6 @@
 
+const tareasProgramadas = new Map();
+
 const CaracteristicasPorDefecto = {
     velocidad: 100,
     cursor: '_',
@@ -23,8 +25,11 @@ function crearEstructuraHTML(donde, caracteristicas){
 
     elementoDestino.innerHTML = `
         <span class="estiloTexto   ${caracteristicas.estiloTexto??''}"></span
-        ><span class="estiloCursor ${caracteristicas.estiloCursor??''} 
-                                   ${caracteristicas.velocidadParpadeoCursor > 0 ? 'parpadeante' : '' }"
+        ><span class="estiloCursor ${caracteristicas.estiloCursor??''}"
+               style="${
+                    caracteristicas.velocidadParpadeoCursor > 0 ? 
+                        'animation: cursor '+ caracteristicas.velocidadParpadeoCursor+'ms infinite;': ''
+               }"
                                   >${caracteristicas.cursor}</span>
     `;
     return elementoDestino;
@@ -33,8 +38,21 @@ function crearEstructuraHTML(donde, caracteristicas){
 
 async function comenzarAEscribirTexto(elemento, textoAEscribir, caracteristicas){
     const promise = new Promise( 
-        (resolve, reject) => procesarSiguienteLetra(elemento, textoAEscribir, 1, caracteristicas, resolve) 
+        (resolve, reject) => {
+            procesarSiguienteLetra(elemento, textoAEscribir, 1, caracteristicas, resolve) 
+            ejecutarFuncionSiUnElementoEsBorrado(elemento, 
+                ()=> {
+                    if (tareasProgramadas.get(elemento)){
+                        clearTimeout(tareasProgramadas.get(elemento));
+                        reject();
+                    }
+                }
+            );
+        
+        }
     );
+
+
     return promise;
 
     // Opción 1: Bucle!... para cada letra, la añado... esperando cadencia
@@ -46,6 +64,7 @@ async function comenzarAEscribirTexto(elemento, textoAEscribir, caracteristicas)
 }
 
 function procesarSiguienteLetra(elemento, textoAEscribir, posicionFinal, caracteristicas, resolve){
+    tareasProgramadas.delete(elemento);
     escribirEnElHTMLHastaLaLetra(elemento, textoAEscribir, posicionFinal);
     siHayMasLetrasSeguirEscribiendo(elemento, textoAEscribir, posicionFinal, caracteristicas, resolve);
     siNoHayMasLetrasFinalizar(elemento, textoAEscribir, posicionFinal, caracteristicas, resolve);
@@ -53,10 +72,11 @@ function procesarSiguienteLetra(elemento, textoAEscribir, posicionFinal, caracte
 
 function siHayMasLetrasSeguirEscribiendo(elemento, textoAEscribir, posicionFinal, caracteristicas, resolve){
     if (posicionFinal < textoAEscribir.length){ // Hay que escribir más letras
-        setTimeout( 
+        const idTarea = setTimeout( 
             ()=> procesarSiguienteLetra(elemento, textoAEscribir, posicionFinal+1, caracteristicas, resolve)
             ,caracteristicas.velocidad
         );
+        tareasProgramadas.set(elemento, idTarea);
     }
 }
 
@@ -75,4 +95,15 @@ function escribirEnElHTMLHastaLaLetra(elemento, textoAEscribir, posicionFinal){
 function enSuCasoOcultarCursor(elemento, caracteristicas){
     if( caracteristicas.ocultarCursorAlFinalizar )
         elemento.querySelector('.estiloCursor').style.visibility = 'hidden';
+}
+
+function ejecutarFuncionSiUnElementoEsBorrado(elemento, funcionAEjecutar){
+    const observador = new MutationObserver(
+        mutaciones => mutaciones.forEach(
+                mutacion => mutacion.removedNodes.forEach( 
+                    nodoEliminado => nodoEliminado === elemento && funcionAEjecutar()
+                )
+        )
+    );
+    observador.observe(elemento.parentNode, {childList: true, subtree: true});
 }
