a32978a1ea801a5f58f14097d0b5f6483fa3133b
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2025-02-17 18:03:20 +0100

    dia4

---
+++ proyecto3/overlay.js
@@ -0,0 +1,10 @@
+/*<div class="overlay" style="background-color: #dddddddd">
+    <img class="overlay-image" src="overlay.jpg" style="opacity:0.2">
+    <h2 class="overlay-title">Estamos realizando su búsqueda</h2>
+    <p class="overlay-message">Tardaremos solamente unos segundos.</p>
+</div>
+*/
+
+function mostrarOverlay(){
+    console.log("mostrarOverlay");
+}
