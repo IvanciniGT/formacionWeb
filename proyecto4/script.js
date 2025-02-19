e9778464899be465b4cb48ca9ad894e26d203a07
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2025-02-19 16:21:36 +0100

    proyecto overlay

--- proyecto4/script.js
+++ proyecto4/script.js
@@ -41,6 +41,7 @@ function agregarListeners() {
     );
     document.addEventListener("mousemove",
         (informacionDelEvento)=>{
+            /*
             if(moviendoBotonesDelNavegador && elementoAMover){
                 movido = true;
                 let nuevaPosicionX = informacionDelEvento.clientX - posicionNavegador.x;
@@ -64,13 +65,33 @@ function agregarListeners() {
                 elementoAMover.style.left = `${nuevaPosicionX}px`;
                 elementoAMover.style.top = `${nuevaPosicionY}px`;
             }
+                */
         }        
     );
 
 
     document.getElementById("activar").addEventListener("click",(e) =>clickEnBotonNotas(e));
+    
+    document.getElementById("papelera").addEventListener("dragover",() => {
+        console.log("dragover");
+        document.getElementById("papelera").classList.add("open");
+    });
+    
+    document.getElementById("papelera").addEventListener("dragleave",() =>{
+        console.log("dragleave");
+        document.getElementById("papelera").classList.remove("open");
+    });
+    
+    document.getElementById("papelera").addEventListener("drop",(e) =>{
+        console.log("drop", elementoAMover);
+        elementoAMover.remove();
+    });
 }
 function agregarListenersAUnElemento(elemento) {
+    elemento.addEventListener("dragstart", (e) => {
+        console.log("dragstart");
+        e.dataTransfer.setData("text/plain", e.target.id);
+    });
 
     elemento.addEventListener("mousedown",
                 (informacionDelEvento)=>{
@@ -80,15 +101,39 @@ function agregarListenersAUnElemento(elemento) {
                     posicionNavegador.x = informacionDelEvento.offsetX;
                     posicionNavegador.y = informacionDelEvento.offsetY;
                     elemento.style.cursor = "grabbing";
-                    if(informacionDelEvento.target.tagName !== "TEXTAREA")
-                        informacionDelEvento.preventDefault();
+                    //if(informacionDelEvento.target.tagName !== "TEXTAREA")
+                        //informacionDelEvento.preventDefault();
                 }
     );
 
     elemento.addEventListener("mouseup",
         (informacionDelEvento)=>{
+            if(moviendoBotonesDelNavegador && elementoAMover){
+                //movido = true;
+                let nuevaPosicionX = informacionDelEvento.clientX - posicionNavegador.x;
+                let nuevaPosicionY = informacionDelEvento.clientY - posicionNavegador.y;
+                
+                if(nuevaPosicionX < 10) {
+                    nuevaPosicionX = 10;
+                }
+                if(nuevaPosicionY < 10) {
+                    nuevaPosicionY = 10;
+                }
+
+                if(nuevaPosicionX + elementoAMover.offsetWidth + 10 > document.documentElement.clientWidth){
+                    nuevaPosicionX = document.documentElement.clientWidth - elementoAMover.offsetWidth - 10;
+                }
+
+                if(nuevaPosicionY + elementoAMover.offsetHeight + 10 > document.documentElement.clientHeight){
+                    nuevaPosicionY = document.documentElement.clientHeight - elementoAMover.offsetHeight - 10;
+                }
+
+                elementoAMover.style.left = `${nuevaPosicionX}px`;
+                elementoAMover.style.top = `${nuevaPosicionY}px`;
+            }
             moviendoBotonesDelNavegador = false;
             elemento.style.cursor = "pointer";
+
         }
     );
 
